0d87824aab6f47955f076d147853fe80b6f1347e
Jubair Ahmed Nabin <ahmednabin.csecuet@gmail.com>
2021-08-29 01:48:57 +0600

    Add about.php

---
+++ about.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>About</title>
+        <link rel="stylesheet" type="text/css" href="css/about-style.css">
+        <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        
+</head>
+    <body>
+            <div class="row">
+                <div class="col-sm-7">
+                    <h2 class="text-center">CUET Central Library </h2>
+                    <p><b>READING FACILITIES</b><br><br>The entire Ground and 1st floor of the library is dedicated for individual study and group discussion of the students.Multiple copies of different text books are available in the counter. The students can issue books on production of ID card and library card from the counter and use these within the library and are not eligible for check out. Approximately 200 students can use these facilities at a time. There is also provision for 60 individual study of researchers, faculty members and post-graduate students in the first floor.<br><br><b>AIR CONDITIONED ROOM:</b><br><br>The library is fully central air conditioned. So the students can study here and feel ease.<br><br><b>NEWSPAPERS AND MAGAZINES:</b><br><br>The daily newspapers are available here. There are also some monthly magazines here. So the students get the chance to read these.
+                    </p>
+                </div>
+                <div class="col-sm-5">
+                    <div class="img-wrap">
+                        <img src="img/mission.jpg">
+                    </div> 
+                </div>
+            </div>
+    </body>
+</html>
