70c2ffcf833d8b087c22303bd43614680ab1266e
Jubair Ahmed Nabin <ahmednabin.csecuet@gmail.com>
2021-08-30 22:03:45 +0600

    Update code formatting

--- faculty/faculty-signup.php
+++ faculty/faculty-signup.php
@@ -1,28 +1,30 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Faculty Signup</title>
-        <link rel="stylesheet" href="css/faculty-signup-style.css">
-    </head>
 
-    <body>
-        <divc class="signupbox">
-            <img src="img/1200px-CUET_Vector_ogo.svg.png" class="image">
-            <h1>Faculty Signup here!</h1>
+<head>
+    <title>Faculty Signup</title>
+    <link rel="stylesheet" href="css/faculty-signup-style.css">
+</head>
 
-            <form action="http://localhost/library-website/faculty/faculty-reg.php" method="POST">
-                <p>Username</p>
-                <input type="text" name="faculty_username" placeholder="Enter username" >
-                <p>Faculty ID</p>
-                <input type="number" name="facultyid" placeholder="Enter Faculty ID" >
-                <p>Department</p>
-                <input type="text" name="department" placeholder="Enter Department" >
-                <p>Email ID</p>
-                <input type="text" name="emailid" placeholder="Enter Email ID" >
-                <p>Password</p>
-                <input type="password" name="password" placeholder="Enter password" >
-                <input type="submit" name="" value="Signup">
-            </form>
-        </divc>
-    </body>
+<body>
+    <divc class="signupbox">
+        <img src="img/1200px-CUET_Vector_ogo.svg.png" class="image">
+        <h1>Faculty Signup here!</h1>
+
+        <form action="http://localhost/library-website/faculty/faculty-reg.php" method="POST">
+            <p>Username</p>
+            <input type="text" name="faculty_username" placeholder="Enter username">
+            <p>Faculty ID</p>
+            <input type="number" name="facultyid" placeholder="Enter Faculty ID">
+            <p>Department</p>
+            <input type="text" name="department" placeholder="Enter Department">
+            <p>Email ID</p>
+            <input type="text" name="emailid" placeholder="Enter Email ID">
+            <p>Password</p>
+            <input type="password" name="password" placeholder="Enter password">
+            <input type="submit" name="" value="Signup">
+        </form>
+    </divc>
+</body>
+
 </html>
