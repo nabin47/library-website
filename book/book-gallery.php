eb3b39da26350e997278991693e1c207d467d2c9
Jubair Ahmed Nabin <ahmednabin.csecuet@gmail.com>
2021-08-30 19:04:45 +0600

    Add the back button

--- book/book-gallery.php
+++ book/book-gallery.php
@@ -6,6 +6,7 @@
 <link rel="stylesheet" type="text/css" href="css/book-gallery-style.css">
 </head>
 <body>
+<a class="back-button" href="javascript:history.go(-1)"onMouseOver="self.status.referrer;return true"><button class="back">Back</button></a>
 <div class="album-sec">
 <div class="container">
 <div class="row">
@@ -27,7 +28,7 @@
 <h3>Intel microprocessor</h3>
 <p>Barry B. Brey</p>
 </div>
-<a href="bookgallery.php?file=1349-intel_book.pdf" target="_blank">download</a>
+<a href="book-gallery.php?file=1349-intel_book.pdf" target="_blank">download</a>
 </div>
 </div>
 </div>
@@ -41,7 +42,7 @@
 <h3>Textbook of Electrical Technology	</h3>
 <p>BL Theraja</p>
 </div>
-<a href="BooksGallery.php?file=3966-a-textbook-of-electrical-technology-volume-i-basic-electrical-engineering-b-l-theraja.pdf" target="_blank">download</a>
+<a href="book-gallery.php?file=3966-a-textbook-of-electrical-technology-volume-i-basic-electrical-engineering-b-l-theraja.pdf" target="_blank">download</a>
 </div>
 </div>
 </div>
@@ -55,7 +56,7 @@
 <h3>Discrete mathematics and its applications</h3>
 <p>Kenneth H. Rosen</p>
 </div>
-<a href="BooksGallery.php?file=4305-Rosen_Discrete_Mathematics_and_Its_Applications_7th_Edition.pdf" target="_blank">download</a>
+<a href="book-gallery.php?file=4305-Rosen_Discrete_Mathematics_and_Its_Applications_7th_Edition.pdf" target="_blank">download</a>
 </div>
 </div>
 </div>
@@ -69,7 +70,7 @@
 <h3>Introduction to automata theory, languages, and computation</h3>
 <p>John E. Hopcroft</p>
 </div>
-<a href="BooksGallery.php?file=7842-John E. Hopcroft, Rajeev Motwani, Jeffrey D. Ullman-Introduction to Automata Theory, Languages, and Computations-Prentice Hall (2006).pdf" target="_blank">download</a>
+<a href="book-gallery.php?file=7842-John E. Hopcroft, Rajeev Motwani, Jeffrey D. Ullman-Introduction to Automata Theory, Languages, and Computations-Prentice Hall (2006).pdf" target="_blank">download</a>
 </div>
 </div>
 </div>
@@ -83,7 +84,7 @@
 <h3>INTRODUCTORY CIRCUIT ANALYSIS/h3>
 <p>Robert L. Boylestad</p>
 </div>
-<a href="BooksGallery.php?file=8596-INTRODUCTORY CIRCUIT ANALYSIS.PDFpdf" target="_blank">download</a>
+<a href="book-gallery.php?file=8596-INTRODUCTORY CIRCUIT ANALYSIS.PDFpdf" target="_blank">download</a>
 </div>
 </div>
 </div>
@@ -97,7 +98,7 @@
 <h3>Principle of Electronics</h3>
 <p>V.K Mehta & Rohit Mehta</p>
 </div>
-<a href="BooksGallery.php?file=9132-principles-of-electronics-s-chand-v-k-mehta-rohit-mehta.pdf" target="_blank">download</a>
+<a href="book-gallery.php?file=9132-principles-of-electronics-s-chand-v-k-mehta-rohit-mehta.pdf" target="_blank">download</a>
 </div>
 </div>
 </div>
@@ -111,7 +112,7 @@
 <h3>C++Complete Reference</h3>
 <p>Herbert Schildt</p>
 </div>
-<a href="BooksGallery.php?file=1512-C++Complete Reference (3rd Ed.).pdf" target="_blank">download</a>
+<a href="book-gallery.php?file=1512-C++Complete Reference (3rd Ed.).pdf" target="_blank">download</a>
 </div>
 </div>
 </div>
@@ -125,7 +126,7 @@
 <h3>Fundamentals Of Electric Circuits</h3>
 <p>Charles K. Alexander & Charles Alexander</p>
 </div>
-<a href="BooksGallery.php?file=9615-Fundamentals_Of_Electric_Circuits-5th-Edition.pdf" target="_blank">download</a>
+<a href="book-gallery.php?file=9615-Fundamentals_Of_Electric_Circuits-5th-Edition.pdf" target="_blank">download</a>
 </div>
 </div>
 </div>
@@ -139,7 +140,7 @@
 if(!empty($_GET['file']))
 {
   $filename = basename($_GET['file']);
-$filepath = 'document/' . $filename;
+$filepath = '../document/' . $filename;
     if(!empty($filename)&&file_exists($filepath)){
         header("Cache-Control: public");
         header("Content-Description: File Transfer");
