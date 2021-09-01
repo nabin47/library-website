91dedf4fc16642754f47624fb28df4f879afa2ea
Jubair Ahmed Nabin <ahmednabin.csecuet@gmail.com>
2021-09-01 19:14:29 +0600

    Add responsive nav

--- book/view-book.php
+++ book/view-book.php
@@ -4,7 +4,6 @@ $dbusername = "root";
 $dbpassword = "";
 $dbname = "cuetcentrallibrary";
 
-// Establish connection
 $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
 ?>
 <!DOCTYPE html>
@@ -12,11 +11,12 @@ $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
 
 <head>
     <title>Books</title>
+
     <link rel="stylesheet" href="css/view-book-style.css">
     <style type="text/css">
         table {
             border-collapse: collapse;
-            min-width: 50vw;
+            width: 100%;
             font-size: 18px;
             text-align: center;
         }
@@ -37,32 +37,64 @@ $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
             top: 10px;
             text-align: center;
             position: absolute;
-            top: 10%;
+            top: 15%;
             left: 50%;
             transform: translate(-50%, -50%);
         }
     </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
 </head>
 
 <body>
-    <!-- <header> -->
+    <header>
         <!--___________________search bar________________________-->
 
-        <div class="srch">
-            <form class="navbar-form" method="post" name="form1">
-                <input class="form-control" type="text" name="search" placeholder="Search Books..." required="">
-                <button type="submit" name="submit">
+        <!-- <div class="srch">
+		<form class="navbar-form" method="post" name="form1">
+				<input class="form-control" type="text" name="search" placeholder="Search Books..." required="">
+				<button type="submit" name="submit">
                     Search
-                    <!--<span class="glyphicon glyphicon-search"></span> -->
-                </button>
-            </form>
-        </div>
-        <div class="button">
-            <a href="javascript:history.go(-1)" onMouseOver="self.status.referrer;return true" class="btn">Back</a>
-        </div>
+					
+				</button>
+		</form>
+	</div>
+	      <div class="button">
+                    <a href="javascript:history.go(-1)"onMouseOver="self.status.referrer;return true" class="btn">Back</a>
+                </div> -->
 
-        <h1>List of books</h1>
+
+        <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #063247">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="../img/logo.png" alt="" width="35" height="50">
+                </a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarToggler">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+
+                    </ul>
+                    <form class="navbar-form d-flex" method="post" name="form1">
+                        <input class="form-control me-2" type="text search" name="search" placeholder="Search Books..." required="" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit" name="submit">
+                            Search
+
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+
+        
         <div class="tb">
+            <h1>List of books</h1>
 
             <!-- IF the search button is pressed-->
             <?php
@@ -194,7 +226,8 @@ $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
 
             ?>
         </div>
-    <!-- </header> -->
+    </header>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 </body>
 
 </html>
