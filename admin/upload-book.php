394a63c217d1566037063bd8361a22914876a673
Jubair Ahmed Nabin <ahmednabin.csecuet@gmail.com>
2021-09-08 20:52:22 +0600

    Show error message while uploading nothing

--- admin/upload-book.php
+++ admin/upload-book.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>File upload</title>
-        <link rel="stylesheet" href="css/upload-book-style.css">
-    </head>
-    <body>
-            <divc class="signupbox">
-            <img src="img/1200px-CUET_Vector_ogo.svg.png" class="image">
-            <h1>Upload Book's here!</h1>
-            <form method="post" enctype="multipart/form-data">
+
+<head>
+    <title>File upload</title>
+    <link rel="stylesheet" href="css/upload-book-style.css">
+</head>
+
+<body>
+    <divc class="signupbox">
+        <img src="img/1200px-CUET_Vector_ogo.svg.png" class="image">
+        <h1>Upload Book's here!</h1>
+        <form method="post" enctype="multipart/form-data">
             <p>ID</p>
             <input type="text" name="id" placeholder="Enter Book ID">
             <p>Book Name</p>
             <input type="text" name="title" placeholder="Book Name">
-            <p>Authors<p>
-            <input type="text" name="author" placeholder="Enter Auhtor's Name">
+            <p>Authors
+            <p>
+                <input type="text" name="author" placeholder="Enter Auhtor's Name">
             <p>Edition</p>
             <input type="text" name="edition" placeholder="Enter Edition">
             <p>Status</p>
@@ -27,42 +30,50 @@
             <input type="file" name="file">
             <input type="submit" name="submit">
         </form>
-        </divc>
-    </body>
+    </divc>
+</body>
+
 </html>
 <?php
-    $host = "localhost";
-    $dbusername = "root";
-    $dbpassword = "";
-    $dbname = "cuetcentrallibrary";
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "cuetcentrallibrary";
 
-    $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
-    if(isset($_POST["submit"]))
-    {
-        $id=$_POST["id"];
-        $title=$_POST["title"];
-        $author=$_POST["author"];
-        $edition=$_POST["edition"];
-        $status=$_POST["status"];
-        $quantity=$_POST["quantity"];
-        $department=$_POST["department"];
-        $pname=rand(1000,10000)."-".$_FILES["file"]["name"];
-        $tname=$_FILES["file"]["tmp_name"];
-        $uploads_dir='C:\xampp\htdocs\library-website\document';
-        move_uploaded_file($tname,$uploads_dir.'/'.$pname);
-        $sql="INSERT into fileup(id,title,author,edition,status,quantity,department,document) VALUES('$id','$title','$author','$edition','$status','$quantity','$department','$pname')";
-        
-        if(mysqli_query($conn,$sql)){?>
+$conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+if (isset($_POST["submit"])) {
+    if (empty($_POST["id"]) || empty($_POST["title"]) || empty($_POST["status"]) || empty($_POST["quantity"]) || empty($_FILES["file"]["name"])) { ?>
+        <script type="text/javascript">
+            alert("Please enter necessary information and the file!")
+            window.location = "upload-book.php"
+        </script>
+        <?php } else {
+        $id = $_POST["id"];
+        $title = $_POST["title"];
+        $author = $_POST["author"];
+        $edition = $_POST["edition"];
+        $status = $_POST["status"];
+        $quantity = $_POST["quantity"];
+        $department = $_POST["department"];
+        $pname = rand(1000, 10000) . "-" . $_FILES["file"]["name"];
+        $tname = $_FILES["file"]["tmp_name"];
+        $uploads_dir = 'C:\xampp\htdocs\library-website\document';
+        move_uploaded_file($tname, $uploads_dir . '/' . $pname);
+        $sql = "INSERT into fileup(id,title,author,edition,status,quantity,department,document) VALUES('$id','$title','$author','$edition','$status','$quantity','$department','$pname')";
+
+
+
+        if (mysqli_query($conn, $sql)) { ?>
             <script type="text/javascript">
-                    alert("Book uploaded successfully!")
-                    window.location = "admin-home.php"
-                </script>
-        <?php }
-        else {?>
+                alert("Book uploaded successfully!")
+                window.location = "admin-home.php"
+            </script>
+        <?php } else { ?>
             <script type="text/javascript">
-                    alert("Approved book successfully!! Great work sir!!")
-                    window.location = "upload-book.php"
-                </script>
-        <?php }
+                alert("Something is wrong! Please Try again!")
+                window.location = "upload-book.php"
+            </script>
+<?php }
     }
+}
 ?>
