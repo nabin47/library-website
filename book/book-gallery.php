7f538ca0ec48d7284ba88c0bfb687462bddec28e
Jubair Ahmed Nabin <ahmednabin.csecuet@gmail.com>
2021-09-17 23:34:04 +0600

    Add new bg img, footer

--- book/book-gallery.php
+++ book/book-gallery.php
@@ -28,34 +28,41 @@ if (!empty($_GET['file'])) {
     <!-- <link rel="stylesheet" type="text/css" href="css/bootstrap.css"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/book-gallery-style.css">
+    <link rel="stylesheet" href="../css/nav-footer-style.css">
+    <link href="../open-iconic/font/css/open-iconic-bootstrap.css" rel="stylesheet">
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark sticky-top" style="background-color: #063247">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="http://localhost/library-website/index.php">
-                <img src="../img/logo.png" alt="CUET logo" width="35" height="50">
-            </a>
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <!-- navbar start -->
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="../index.php"><img src="../img/logo.png" alt="CUET logo" style="height: 10vh; width: auto;"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
-            <div class="collapse navbar-collapse" id="navbarToggler">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <div class="collapse navbar-collapse flex-grow-1 text-right" id="navbarNav">
+                <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="javascript:history.go(-1)">Back</a>
+                        <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
                     </li>
-
+                    <li class="nav-item">
+                        <a class="nav-link" href="../about.php">About</a>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav ms-auto flex-nowrap">
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:history.go(-1)"><span class="oi oi-arrow-left"></span> Back</a>
+                    </li>
                 </ul>
             </div>
         </div>
     </nav>
-    <!-- <div>
-        <a class="back-button" href="http://localhost/library-website/index.php"><button class="back">Back</button></a>
-    </div> -->
+
+    <!-- navbar end -->
+
     <div class="album-sec">
-        <div class="container">
+        <div class="container-fluid">
             <div class="row page-title">
                 <div class="col-md-12">
                     <div class="header text-center">
@@ -181,6 +188,19 @@ if (!empty($_GET['file'])) {
             </div>
         </div>
     </div>
+
+     <!-- footer start -->
+     <footer class="text-white text-center text-lg-start">
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © Developed By:
+            <a class="text-white" href="https://mdbootstrap.com/">Jawad</a> <span> & </span>
+            <a class="text-white" href="https://mdbootstrap.com/">Nabin</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <!-- footer end -->
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 </body>
 
