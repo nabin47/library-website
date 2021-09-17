c2d7d83a13ea44b61e028e151febef7dbc76c0c3
Jubair Ahmed Nabin <ahmednabin.csecuet@gmail.com>
2021-09-17 20:55:42 +0600

    Add carousel-control to make carousel clickable to book gallery

---
+++ js/carousel-control.js
@@ -0,0 +1,6 @@
+// Clicking on carousel opens book gallery
+
+const clickCarousel = document.querySelector('.carousel-item');
+clickCarousel.addEventListener("click", ()=>{
+    window.location="book/book-gallery.php";
+});
