1e3c04c5a52aae9077fb786763d2bca51d4c3681
Jubair Ahmed Nabin <ahmednabin.csecuet@gmail.com>
2021-09-18 00:29:35 +0600

    Add footer and new bg via nav-footer-style

--- book/math-books.php
+++ book/math-books.php
@@ -13,6 +13,7 @@ $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
     <title>Books</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/view-book-style.css">
+    <link rel="stylesheet" href="../css/nav-footer-style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 </head>
 
@@ -192,6 +193,18 @@ $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
 
         ?>
     </div>
+
+    <!-- footer start -->
+    <footer class="text-white text-center text-lg-start">
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © Developed By:
+            <a class="text-white" href="https://mdbootstrap.com/">Jawad</a> <span> & </span>
+            <a class="text-white" href="https://mdbootstrap.com/">Nabin</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <!-- footer end -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 </body>
