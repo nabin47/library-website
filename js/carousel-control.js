ae2b7df6b38f3fe7a22b9eb0cb3a06c98498aa36
Jubair Ahmed Nabin <ahmednabin.csecuet@gmail.com>
2021-09-18 00:07:30 +0600

    Update with forEach loop so that link works in all slides of carousel

--- js/carousel-control.js
+++ js/carousel-control.js
@@ -1,6 +1,6 @@
 // Clicking on carousel opens book gallery
 
-const clickCarousel = document.querySelector('.carousel-item');
-clickCarousel.addEventListener("click", ()=>{
+const clickCarousel = document.querySelectorAll('.carousel-item');
+clickCarousel.forEach(element => element.addEventListener("click", ()=>{
     window.location="book/book-gallery.php";
-});
+}));
