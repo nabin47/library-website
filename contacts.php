90d32f543639ac335e1eaf3c376253d421138071
Jubair Ahmed Nabin <ahmednabin.csecuet@gmail.com>
2021-09-22 01:49:30 +0600

    Add contact page

---
+++ contacts.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Contacts</title>
+    <link rel="stylesheet" href="css/contact-style.css">
+
+</head>
+
+<body>
+    <div class="team-section">
+        <h1>Our Team</h1>
+        <span class="border"></span>
+        <div class="ps">
+            <a href="#p1"><img src="img/39.jpg" alt=""></a>
+            <a href="#p2"><img src="img/nabin.jpg" alt=""></a>
+        </div>
+        <div class="section" id="p1">
+            <span class="name"> Jawad Hossain</span>
+            <span class="border"></span>
+            <p>Jawad Hossain is a undergraduate student of Department of CSE, CUET. He is a member of CUET Computer Club, RMA, IEEE. <br>Contact No. +8801834129910<br>E-mail: jawad.cuet.bd@gmail.com</p>
+        </div>
+        <div class="section" id="p2">
+            <span class="name">Jubair Ahmed Nabin</span>
+            <span class="border"></span>
+            <p>Jubair Ahmed Nabin is a undergraduate student of Department of CSE, CUET. He is a member of CUET Computer Club, RMA, IEEE. <br>Contact No. +8801984702077<br>E-mail: u1704047@student.cuet.ac.bd</p>
+        </div>
+    </div>
+</body>
+
+</html>
