d34988ce90f79c803b19d914ff40d1faea90d0c3
Jubair Ahmed Nabin <ahmednabin.csecuet@gmail.com>
2021-09-22 19:24:56 +0600

    Add gallery page & style

---
+++ gallery.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/photo-gallery-style.css">
+    <title>Gallery</title>
+</head>
+<body>
+    <section>
+        <div class="description">
+            <a href="index.php"><img src="img/logo.png" alt="" class="logo"></a>
+            <a href="https://www.cuet.ac.bd/"><h1>Life At CUET!</h1></a>
+            <p>Take a look at the Unknown side of CUET</p>
+        </div>
+        <div class="photo-gallery">
+            <img src="img/IMG_6059-small.jpg" alt="" class="gallery-highlight">
+            <div class="photo-preview">
+                <img src="img/IMG_6059-small.jpg" alt="" class="photo-active">
+                <img src="img/IMG_0613-small.jpg" alt="">
+                <img src="img/IMG_1851-small.jpg" alt="">
+                <img src="img/IMG_1884-small.jpg" alt="">
+            </div>
+        </div>
+    </section>
+    <script src="js/gallery.js"></script>
+</body>
+</html>
